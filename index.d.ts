45e73e349d44ccb5caae7212bdc84d942622fb55
dengdong <dengd@esrichina.com.cn>
2022-09-23 17:28:05 +0800

    feat✨:  修改类型描述文件

--- index.d.ts
+++ index.d.ts
@@ -3,11 +3,27 @@
  * @Author: travelclover(travelclover@163.com)
  * @Date: 2022-09-23 10:09:03
  */
+export interface Config {
+  /** 线条颜色，十六进制颜色值 */
+  color?: string | string[];
+  /** 流动效果长度占流动路径长度的比例，取值范围大于0并且小于1。 */
+  flowRatio?: number | number[];
+  /** 流动速度 */
+  speed?: number | number[];
+  /** 点密度，在地图缩放层级较大的情况下，该值越大，越呈现出一条连续的线 */
+  density?: number;
+}
+
 export default class FlowLineRenderer {
   view: __esri.SceneView;
   line: __esri.Polyline;
 
-  constructor(view: __esri.SceneView, line: __esri.Polyline, config?: any);
+  constructor(
+    externalRenderers: __esri.externalRenderers,
+    view: __esri.SceneView,
+    line: __esri.Polyline,
+    config?: Config
+  );
 
   /**
    * 更新线
